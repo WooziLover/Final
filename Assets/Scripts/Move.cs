b1be21f07c8ae0ddf05c0288834ac4aba99ad0b0
tl00456 <oohty15@gmail.com>
2019-05-22 14:29:17 -0700

    Rough Draft #1
    Worked on Move Class
    Added some scripts
    Made the object move - just need to work on speed

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    Rigidbody rigidBody;
+    public float speed = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.DownArrow))
+             transform.Translate(0f, 0f, Input.GetAxis("Vertical") * Time.deltaTime);
+         if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow))
+             transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime, 0f, 0f);
+       // if (Input.GetKey(KeyCode.R))
+           // rigidBody.velocity = speed * Time.deltaTime;
+             //rigidBody.velocity = transform.forward * speed;
+       
+    }
+}
