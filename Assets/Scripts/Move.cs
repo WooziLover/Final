df9e85d55aa735a8fddf0c2641f86be69b7baa57
TristynL <oohty15@gmail.com>
2019-05-22 23:01:25 -0700

    RD #2
    Finished Move Class?
    Worked and Almost done with the Rotate Class

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -5,24 +5,34 @@ using UnityEngine;
 public class Move : MonoBehaviour
 {
     Rigidbody rigidBody;
-    public float speed = 5.0f;
+    public float mSpeed;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
+        mSpeed = 1.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-         if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.DownArrow))
-             transform.Translate(0f, 0f, Input.GetAxis("Vertical") * Time.deltaTime);
-         if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow))
-             transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime, 0f, 0f);
-       // if (Input.GetKey(KeyCode.R))
-           // rigidBody.velocity = speed * Time.deltaTime;
-             //rigidBody.velocity = transform.forward * speed;
-       
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.DownArrow))
+            rigidBody.transform.Translate(0f, 0f, Input.GetAxis("Vertical") * mSpeed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow))
+            rigidBody.transform.Translate(Input.GetAxis("Horizontal") * mSpeed * Time.deltaTime, 0f, 0f);
+        if (Input.GetKey(KeyCode.R))
+        {
+            mSpeed += 2.0f;
+            rigidBody.transform.Translate(Input.GetAxis("Horizontal") * mSpeed * Time.deltaTime, 0f, Input.GetAxis("Vertical") * 
+                mSpeed * Time.deltaTime);
+        }
+        else
+        {
+            mSpeed = 1.0f;
+        }
+            
     }
 }
+
+
--- Assets/Scripts/Rotate.cs
+++ Assets/Scripts/Rotate.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class Rotate : MonoBehaviour
 {
+    public float mSpeed;
+    Rigidbody rigidBody;
     // Start is called before the first frame update
     void Start()
     {
-        
+        mSpeed = 3.0f;
+        rigidBody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKey(KeyCode.A))
+            rigidBody.transform.RotateAround(Vector3.zero, Vector3.up, 20 * Time.deltaTime);
+        if (Input.GetKey(KeyCode.D))
+            rigidBody.transform.RotateAround(Vector3.zero, Vector3.down, 20 * Time.deltaTime);
+
+        /*if (Input.GetKey(KeyCode.A))
+        {
+            transform.RotateAround(point, Vector3.up, 20 * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.D))
+            transform.RotateAround(point, -Vector3.up, 20 * Time.deltaTime);*/
     }
 }
