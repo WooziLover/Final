9e196493386916ccf1361c3c65e1ff0269499720
TristynL <oohty15@gmail.com>
2019-05-27 23:08:52 -0700

    Rough Draft 5/27/19
    Created and Worked on the ChooseRandomObject class. Found an issue, get help.
    Check with rotate
    Created a PickUpClass but didn't work on it.

--- Assets/Scripts/ChooseRandomFruit.cs
+++ Assets/Scripts/ChooseRandomFruit.cs
@@ -2,15 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Choose : MonoBehaviour
+public class ChooseRandomFruit : MonoBehaviour
 {
+
+    GameObject[] itemChoices;
+    GameObject currentPoint;
+    int randomChoice;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        itemChoices = GameObject.FindGameObjectsWithTag("point");
+        randomChoice = Random.Range(0, itemChoices.Length);
+        //next line holds the issue where there is a index out of bounds
+      //  currentPoint = itemChoices[randomChoice];
+//        print(currentPoint.name);
     }
 
-    // Update is called once per frame
     void Update()
     {
         
