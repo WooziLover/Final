d723e08336df6fe099b3009b0f960e062ff1223b
tl00456 <oohty15@gmail.com>
2019-05-31 14:26:55 -0700

    Rough Draft 5/31/19
    Well, there were some changes

--- Assets/Scripts/SpawnAndLaunch.cs
+++ Assets/Scripts/SpawnAndLaunch.cs
@@ -7,6 +7,7 @@ public class SpawnAndLaunch : MonoBehaviour
     public GameObject[] itemChoices;
     int direction = 4;
     Rigidbody rigidBody;
+    GameObject spawn;
 
     // Start is called before the first frame update
     void Start()
@@ -19,16 +20,16 @@ public class SpawnAndLaunch : MonoBehaviour
     void Pick()
     {
         int randomIndex = Random.Range(0, itemChoices.Length);
-        GameObject spawn = Instantiate(itemChoices[randomIndex], transform.position, Quaternion.identity);
+       spawn = Instantiate(itemChoices[randomIndex], transform.position, Quaternion.identity);
     }
   
     void Launch()
     {
-        float randomDistance = Random.Range(0, GetComponent<Plane>().distance);
+        
+      /*  float randomDistance = Random.Range(0, GetComponent<Plane>().distance);
         switch (direction)
         {
             case 0:
-               // position.x = randomDistance;
                 rigidBody.AddForce(Vector3.up);
                 break;
 
@@ -43,6 +44,6 @@ public class SpawnAndLaunch : MonoBehaviour
             case 3:
                 rigidBody.AddForce(Vector3.down);
                 break;
-        }
+        }*/
     }
 }
